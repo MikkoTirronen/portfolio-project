1dcb56a451df428716f24ff52d7d359bc070ed33
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-11-30 22:45:47 +0100

    added hero component and css

---
+++ js/components/hero.js
@@ -0,0 +1,10 @@
+//Hero functional component
+
+export function Hero() {
+  return ` 
+        <div class="hero-overlay">
+            <h1 class="title">Mikko Tirronen</h1>
+            <p class="subtitle">Full Stack Developer</p>
+        <div>
+    `;
+}
