b8ccb19b0baf0c6a17f5214b7e138e71606e8b1a
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-12-02 22:49:12 +0100

    added skills render to event listener

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 import { renderComponent } from "./core/render.js";
 import { Hero } from "./components/hero.js";
 import { About } from "./components/about.js";
+import { Skills } from "./components/skills.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   renderComponent("#hero", Hero);
   renderComponent("#about", About);
+  renderComponent("#skills", Skills);
 });
