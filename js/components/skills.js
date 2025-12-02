b9210a70da11a35992267c69543391fea9f825be
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-12-02 22:48:06 +0100

    added skills component that returns skill content

---
+++ js/components/skills.js
@@ -0,0 +1,55 @@
+export function Skills() {
+  return `
+        <h2>Skills</h2>
+        <div class="skills-grid">
+          <div class="skill">
+            <img
+              src="./assets/icons/javascript.jpeg"
+              alt="javascript icon"
+            /><span>JavaScript</span>
+          </div>
+          <div class="skill">
+            <img
+              src="./assets/icons/typescript.png"
+              alt="TypeScript icon"
+            /><span>TypeScript</span>
+          </div>
+          <div class="skill">
+            <img
+              src="./assets/icons/node.jpeg"
+              alt="Node.js icon"
+            /><span>Node.js</span>
+          </div>
+          <div class="skill">
+            <img
+              src="./assets/icons/react.png"
+              alt="React icon"
+            /><span>React</span>
+          </div>
+          <div class="skill">
+            <img
+              src="./assets/icons/mongodb.png"
+              alt="MongoDB icon"
+            /><span>MongoDB</span>
+          </div>
+          <div class="skill">
+            <img
+              src="./assets/icons/postgres.png"
+              alt="Postgres icon"
+            /><span>Postgres</span>
+          </div>
+          <div class="skill">
+            <img
+              src="./assets/icons/csharp.png"
+              alt="C# icon"
+            /><span>C#</span>
+          </div>
+          <div class="skill">
+            <img
+              src="./assets/icons/azure.jpeg"
+              alt="azure icon"
+            /><span>Azure</span>
+          </div>
+        </div>
+        `;
+}
