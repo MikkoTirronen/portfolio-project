96225ea54f129e53271f5e912f9c4c0c040108ae
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-12-04 13:36:07 +0100

    added Navbar functional component

---
+++ js/components/navbar.js
@@ -0,0 +1,23 @@
+export function Navbar() {
+  return `
+          <img
+            src="/assets/favicon-cropped.png"
+            alt="Mikko Tirronen"
+            width="auto"
+            height="36"
+          />
+        <div class="nav-links" role="menubar" aria-label="Sections">
+          <a href="#about" data-link class="active">About</a>
+          <a href="#skills" data-link>Skills</a>
+          <a href="#projects" data-link>Projects</a>
+          <a href="#contact" data-link>Contact</a>
+        </div>
+          <button
+            id="themeToggle"
+            class="theme-toggle"
+            aria-label="Toggle theme"
+          >
+            🌙
+          </button>
+    `;
+}
