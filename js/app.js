ad4430e1e2074a30109417100d08716d3975d300
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-12-09 21:58:24 +0100

    added contact render to dom loaded listener

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ import { About } from "./components/about.js";
 import { Skills } from "./components/skills.js";
 import { Navbar } from "./components/navbar.js";
 import { Projects } from "./components/projects.js";
-
+import { Contact } from "./components/contact.js";
 
 document.addEventListener("DOMContentLoaded", () => {
  renderComponent("#nav", Navbar)
@@ -12,5 +12,5 @@ document.addEventListener("DOMContentLoaded", () => {
   renderComponent("#about", About);
   renderComponent("#skills", Skills);
   renderComponent("#projects", Projects);
-
+  renderComponent("#contact", Contact);
 });
