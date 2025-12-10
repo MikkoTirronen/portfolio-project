1708beca05ec498309994d1b5437d4847bbda77d
MikkoTirronen <MikkoATirronen@gmail.com>
2025-12-10 18:07:40 +0100

    replaced email with character key

--- js/components/contact.js
+++ js/components/contact.js
@@ -1,7 +1,7 @@
 export function Contact() {
   return `
         <h2>Contact</h2>
-        <form action="https://formsubmit.co/YOUR_EMAIL_HERE" method="POST" id="contactForm">
+        <form action="https://formsubmit.co/3d14bd33977bc7783400307772e4d54e" method="POST" id="contactForm">
   
         <!-- Name -->
         <input type="text" name="name" placeholder="Your Name" required />
@@ -15,10 +15,9 @@ export function Contact() {
         <!-- Hidden Settings -->
         <input type="hidden" name="_captcha" value="false" />
         <input type="hidden" name="_template" value="box" />
-        <input type="hidden" name="_next" value="https://YOURDOMAIN.com/success.html" />
+        // <input type="hidden" name="_next" value="http://localhost:5500" />
 
         <button type="submit">Send Message</button>
         </form>
-
     `;
 }
