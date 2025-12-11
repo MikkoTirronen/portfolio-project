9667326a7961d1b24ae8d2cc9ef014001971688d
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-12-11 19:51:17 +0100

    added footer content

---
+++ js/components/footer.js
@@ -0,0 +1,36 @@
+export function Footer() {
+    return `
+    <div class="footer-container">
+
+    <div class="footer-left">
+      <h3 class="footer-brand">Mikko Tirronen</h3>
+      <p class="footer-description">
+        Building clean, modern, and user-focused digital experiences.
+      </p>
+    </div>
+
+    <div class="footer-links">
+      <h4 class="footer-title">Quick Links</h4>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </div>
+
+    <div class="footer-socials">
+      <h4 class="footer-title">Social</h4>
+      <ul>
+        <li><a href="#">LinkedIn</a></li>
+        <li><a href="#">GitHub</a></li>
+        <li><a href="#">Twitter</a></li>
+      </ul>
+    </div>
+
+  </div>
+
+  <div class="footer-bottom">
+    <p>© 2025 Mikko Tirronen. All rights reserved.</p>
+  </div>
+    `
+}
