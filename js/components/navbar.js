6b2f38d901b02c4da5628ea4552946bc45b2df77
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-12-15 15:51:18 +0100

    placed the darktheme toggle inside the nav, added toggle for burger menu

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,4 +1,4 @@
-export function Navbar() {
+export function Navbar({ children }) {
   return `
           <img
             src="/assets/favicon-cropped.png"
@@ -6,20 +6,31 @@ export function Navbar() {
             width="auto"
             height="36"
           />
-        <div class="nav-links" role="menubar" aria-label="Sections">
-          <a href="#about" data-link class="active">About</a>
-          <a href="#skills" data-link>Skills</a>
-          <a href="#projects" data-link>Projects</a>
-          <a href="#testamonials" data-link>Testimonials</a>
-          <a href="#contact" data-link>Contact</a>
-        </div>
+          <div id="navLinks" class="nav-links" role="menubar" aria-label="Sections">
+            <a href="#about" data-link class="active">About</a>
+            <a href="#skills" data-link>Skills</a>
+            <a href="#projects" data-link>Projects</a>
+            <a href="#testamonials" data-link>Testimonials</a>
+            <a href="#contact" data-link>Contact</a>
+          </div>
+          <button id="themeToggle" class="theme-toggle" aria-label="Toggle theme">
+          🌙
+        </button>
+          <button
+          class="nav-toggle"
+          aria-label="Toggle navigation"
+          aria-expanded="false"
+          aria-controls="navLinks"
+          >
+            ☰
+          </button>
         `;
 }
 
-        // <button
-        //   id="themeToggle"
-        //   class="theme-toggle"
-        //   aria-label="Toggle theme"
-        // >
-        //   🌙
-        // </button>
+// <button
+//   id="themeToggle"
+//   class="theme-toggle"
+//   aria-label="Toggle theme"
+// >
+//   🌙
+// </button>
