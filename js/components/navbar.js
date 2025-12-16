9779e16da726f36d1ff8ad1e612a04adbd3b1bf9
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-12-16 22:33:08 +0100

    fixed link after namechange, added styling

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -12,14 +12,14 @@ export function Navbar({ children }) {
             <a href="#about" data-link class="active">About</a>
             <a href="#skills" data-link>Skills</a>
             <a href="#projects" data-link>Projects</a>
-            <a href="#testamonials" data-link>Testimonials</a>
+            <a href="#testimonials" data-link>Testimonials</a>
             <a href="#contact" data-link>Contact</a>
           </div>
-          <button id="themeToggle" class="theme-toggle" aria-label="Toggle theme">
+          <button id="themeToggle" class="theme-toggle toggle-hover" aria-label="Toggle theme">
           🌙
         </button>
           <button
-          class="nav-toggle"
+          class="nav-toggle btn"
           aria-label="Toggle navigation"
           aria-expanded="false"
           aria-controls="navLinks"
