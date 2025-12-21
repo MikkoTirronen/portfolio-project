960325b7172aed0bb6b36c96c218d5808455f0f2
Mikko Tirronen <mikkoatirronen@gmail.com>
2025-12-21 22:16:32 +0100

    added id to weather widget

--- js/components/hero.js
+++ js/components/hero.js
@@ -4,22 +4,21 @@ export function Hero() {
   requestAnimationFrame(GetWeather);
 
   return `
+    <div id="weather-widget" class="weather-widget" aria-label="Weather in Stockholm">
+      <div class="weather-icon-wrapper">
+        <div id="weatherIcon"></div>
+      </div>
 
-  <div class="weather-widget" aria-label="Weather in Stockholm">
-  <div class="weather-icon-wrapper">
-    <div id="weatherIcon"></div>
-  </div>
-
-  <div class="weather-text">
-    <span class="weather-temp">Stockholm</span>
-    <span class="weather-temp" id="weatherTemp">25°C</span>
-    <span class="weather-desc" id="weatherDesc">Sunny</span>
-  </div>
-  </div>
-  <div class="hero-content">
-  <h1 id="hero-heading" class="hero-title">Mikko Tirronen</h1>
-  <p class="hero-subtitle">Junior Software Developer</p>
-  </div>
+      <div class="weather-text">
+        <span class="weather-temp">Stockholm</span>
+        <span class="weather-temp" id="weatherTemp">25°C</span>
+        <span class="weather-desc" id="weatherDesc">Sunny</span>
+      </div>
+    </div>
+    <div class="hero-content">
+      <h1 id="hero-heading" class="hero-title">Mikko Tirronen</h1>
+      <p class="hero-subtitle">Junior Software Developer</p>
+    </div>
   `;
 }
 {/* 
